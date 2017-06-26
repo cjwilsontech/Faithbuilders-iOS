b9fd0b3c57a00cb7d1ecc45b78eeefd68827316d
Curtis Wilson <cj@wilsontech.com>
2017-06-26 09:31:04 -0700

    Added upside-down orientation support.

---
+++ Faithbuilders/TabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  Faithbuilders
+//
+//  Created by Curtis Wilson on 6/26/17.
+//  Copyright © 2017 Eusebeia. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController :UITabBarController {
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        return .all
+    }
+}
